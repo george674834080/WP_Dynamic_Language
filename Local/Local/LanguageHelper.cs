9c2b80ac6c24ece06613218e8cf39fa0488f5b14
im_wower <george674834080@gmail.com>
2013-07-21 03:26:57 +0800

    init

---
+++ Local/Local/LanguageHelper.cs
@@ -0,0 +1,75 @@
+﻿using System.ComponentModel;
+using System.Globalization;
+using System.Threading;
+using Local.Resources;
+
+namespace Local
+{
+    public class LanguageHelper : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void SetLanguage()
+        {
+            Helper.InitLanguage();
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs("Item[]"));
+        }
+
+        public string this[string key]
+        {
+            get
+            {
+                return Helper.GetString(key);
+            }
+        }
+
+        /// <summary>
+        /// for dynamic strings, used in code behind(.cs file)
+        /// </summary>
+        /// <param name="key"></param>
+        /// <param name="parameters"></param>
+        /// <returns></returns>
+        public static string GetString(string key, params object[] parameters)
+        {
+            return string.Format(Helper.GetString(key), parameters);
+        }
+
+        /// <summary>
+        /// singleton
+        /// </summary>
+        private class Helper
+        {
+            #region property
+            private static bool isInit;
+            #endregion
+
+            public static void InitLanguage()
+            {
+                isInit = false;
+            }
+
+            public static string GetString(string key)
+            {
+                if (!isInit)
+                    InitCurrentCulture();
+                return AppResources.ResourceManager.GetString(key);
+            }
+
+            #region private method
+            private static void InitCurrentCulture()
+            {
+                //change language here:
+                var ci = CultureInfo.CurrentUICulture;
+                if (!string.IsNullOrEmpty(App.CurrentLanguage))
+                    ci = new CultureInfo(App.CurrentLanguage);
+                else
+                    App.CurrentLanguage = ci.Name;
+                Thread.CurrentThread.CurrentCulture = ci;
+                Thread.CurrentThread.CurrentUICulture = ci;
+                isInit = true;
+            }
+            #endregion
+        }
+    }
+}
---
+++ Local/Local/MainPage.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using Microsoft.Phone.Controls;
+using System.Globalization;
+
+namespace Local
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        int counter = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (!string.IsNullOrEmpty(App.CurrentLanguage))
+                App.CurrentLanguage = App.CurrentLanguage == "zh-CN" ? "en-US" : "zh-CN";
+
+            //must use current instance:
+            var helper = Application.Current.Resources["LanguageHelper"] as LanguageHelper;
+            helper.SetLanguage();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            counter += 1;
+            MessageBox.Show(LanguageHelper.GetString("Hello", counter));
+        }
+    }
+}
